ce50a92e2dc6ee1baa4a50cdbf4583a82d3e3884
Anders Wind <aws@netcompany.com>
2017-03-20 12:31:00 +0100

    Added the initial files for Lab7

---
+++ Exercises/src/Lab7/Main.java
@@ -0,0 +1,13 @@
+package Lab7;
+
+/**
+ * Created by aws on 20-03-2017.
+ */
+public class Main {
+
+    public static void main(String[] args){
+        SOMDemoApp demo = new SOMDemoApp();
+        demo.setVisible(true);
+        demo.go();
+    }
+}
