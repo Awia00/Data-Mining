b27aa280da27d6377bc04f4b3e49d40f26fa7fc5
Anders Wind <aws@netcompany.com>
2017-05-04 13:18:09 +0200

    added minpath

--- NeatBFS/src/NeatBFS/Graph/ManualShortestPathInstanceFactory.cs
+++ NeatBFS/src/NeatBFS/Graph/ManualShortestPathInstanceFactory.cs
@@ -4,13 +4,19 @@ namespace NeatBFS.Graph
 {
     public class ManualShortestPathInstanceFactory : IShortestPathInstanceFactory
     {
+        public int MinPathLength { get; }
         private readonly IGraph _graph;
 
         public ManualShortestPathInstanceFactory(IGraph graph)
         {
             _graph = graph;
         }
-        
+        public ManualShortestPathInstanceFactory(IGraph graph, int minPathLength=1)
+        {
+            _graph = graph;
+            MinPathLength = minPathLength;
+        }
+
         public IEnumerable<ShortestPathTaskInstance> GenerateInstances()
         {
             for (var source = 0; source < _graph.NumberOfVertices; source++)
@@ -18,7 +24,7 @@ namespace NeatBFS.Graph
                 var distance = _graph.DistanceToArray(source);
                 for (var goal = 0; goal < _graph.NumberOfVertices; goal++)
                 {
-                    if (goal != source && distance[goal] > 2)
+                    if (goal != source && distance[goal] >= MinPathLength)
                     {
                         yield return new ShortestPathTaskInstance
                         {
